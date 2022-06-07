e931d918d3813414f0236b98d63e57e2d6a9e2ef
Abdullah Hinnawi <abdullah.hinnawi@kiva.fi>
2022-06-07 15:31:29 +0300

    Fix hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,10 @@ export default function handler(req, res) {
     res.status(200).json({ text: 'Hello' });
 }
 
+
+/*
 export default function emailHandler(req, res) {
     const email = req.body.email;
     // Then save email to your database, etc...
   }
+*/
